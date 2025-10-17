e22e76072eea97029a84bf4c00843da24470d0b3
FireML <fireml@firedev.uk>
2025-10-17 21:12:34 +0100

    Fix radius and clean up messaging

--- src/main/java/uk/firedev/chatchannels/api/ChatChannel.java
+++ src/main/java/uk/firedev/chatchannels/api/ChatChannel.java
@@ -2,26 +2,19 @@ package uk.firedev.chatchannels.api;
 
 import io.papermc.paper.event.player.AsyncChatEvent;
 import net.kyori.adventure.sound.Sound;
-import net.kyori.adventure.text.Component;
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import uk.firedev.chatchannels.ChatChannels;
-import uk.firedev.chatchannels.configs.MessageConfig;
 import uk.firedev.chatchannels.data.PlayerData;
 import uk.firedev.daisylib.command.CooldownHelper;
 import uk.firedev.daisylib.config.ConfigBase;
 import uk.firedev.daisylib.libs.messagelib.message.ComponentMessage;
 import uk.firedev.daisylib.libs.messagelib.message.ComponentSingleMessage;
 
-import java.time.Duration;
-import java.util.List;
-
 public abstract class ChatChannel extends ConfigBase {
 
-    private final CooldownHelper pingCooldown = CooldownHelper.create();
+    protected final CooldownHelper pingCooldown = CooldownHelper.create();
 
     public ChatChannel(@NotNull String fileName) {
         super(fileName, fileName, ChatChannels.getInstance());
@@ -42,11 +35,11 @@ public abstract class ChatChannel extends ConfigBase {
         return getConfig().getBoolean("ping.enable", true);
     }
 
-    public @Nullable Sound getPingSound() {
+    public @Nullable Sound pingSound() {
         return getSound("ping.sound");
     }
 
-    public int getPingCooldown() {
+    public int pingCooldown() {
         return getConfig().getInt("ping.cooldown");
     }
 
@@ -74,43 +67,7 @@ public abstract class ChatChannel extends ConfigBase {
         }
         ComponentSingleMessage message = format().parsePlaceholderAPI(sender)
             .replace("{name}", sender.name());
-        List<Entity> entitiesWithinRadius = getEntitiesWithinRadius(sender);
-        if (radius() > 0 && entitiesWithinRadius.isEmpty()) {
-            MessageConfig.getInstance().getNoNearbyPlayersMessage().send(sender);
-            return;
-        }
-        message.replace("{message}", event.message()).send(Bukkit.getConsoleSender());
-        Bukkit.getOnlinePlayers().forEach(player -> {
-            // Handle radius
-            if (!entitiesWithinRadius.isEmpty() && !entitiesWithinRadius.contains(player)) {
-                return;
-            }
-            // Check if the message should be sent
-            if (!shouldSendToTarget(sender, player)) {
-                return;
-            }
-            // Checks for mentions
-            Component messageContent = retrieveMessage(player, event.message());
-            message.replace("{message}", messageContent).send(player);
-        });
-    }
-
-    private Component retrieveMessage(@NotNull Player player, @NotNull Component component) {
-        if (!enablePing() || pingCooldown.hasCooldown(player.getUniqueId())) {
-            return component;
-        }
-        ComponentSingleMessage message = ComponentMessage.componentMessage(component);
-        String pingFormat = "@" + player.getName();
-        if (message.containsString(pingFormat)) {
-            message = message.replace(pingFormat, "<red>@" + player.getName());
-            Sound pingSound = getPingSound();
-            if (pingSound != null) {
-                player.playSound(pingSound);
-            }
-            pingCooldown.applyCooldown(player.getUniqueId(), Duration.ofSeconds(getPingCooldown()));
-            return message.get();
-        }
-        return component;
+        new Messaging(this).sendMessage(sender, event, message);
     }
 
     public abstract boolean shouldSendToTarget(@NotNull Player player, @NotNull Player target);
@@ -118,14 +75,6 @@ public abstract class ChatChannel extends ConfigBase {
     public boolean hasAccess(@NotNull Player player) {
         String access = accessPermission();
         return access == null || player.hasPermission(access);
-    }
-
-    public List<Entity> getEntitiesWithinRadius(@NotNull Player player) {
-        long radius = radius();
-        if (radius < 0) {
-            return List.of();
-        }
-        return player.getNearbyEntities(radius, radius, radius);
     }
 
 }
---
+++ src/main/java/uk/firedev/chatchannels/api/Messaging.java
@@ -0,0 +1,78 @@
+package uk.firedev.chatchannels.api;
+
+import io.papermc.paper.event.player.AsyncChatEvent;
+import net.kyori.adventure.sound.Sound;
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import uk.firedev.chatchannels.ChatChannels;
+import uk.firedev.chatchannels.configs.MessageConfig;
+import uk.firedev.daisylib.libs.messagelib.message.ComponentMessage;
+import uk.firedev.daisylib.libs.messagelib.message.ComponentSingleMessage;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Optional;
+
+public record Messaging(ChatChannel channel) {
+
+    public Messaging(@NotNull ChatChannel channel) {
+        this.channel = channel;
+    }
+
+    public void sendMessage(@NotNull Player sender, @NotNull AsyncChatEvent event, @NotNull ComponentSingleMessage message) {
+        Bukkit.getScheduler().runTask(ChatChannels.getInstance(), () -> {
+            Optional<List<Player>> targetPlayers = handleRadius(sender);
+            if (targetPlayers.isEmpty()) {
+                return;
+            }
+            message.replace("{message}", event.message()).send(sender, Bukkit.getConsoleSender());
+            targetPlayers.get().forEach(player -> {
+                // Check if the message should be sent
+                if (!channel.shouldSendToTarget(sender, player)) {
+                    return;
+                }
+                // Checks for mentions/pings
+                Component messageContent = processPing(player, event.message());
+                message.replace("{message}", messageContent).send(player);
+            });
+        });
+    }
+
+    public Optional<List<Player>> handleRadius(@NotNull Player sender) {
+        long radius = channel.radius();
+        // If the radius is 0 or less, we can just pass all online players
+        if (radius < 0) {
+            return Optional.of(List.copyOf(Bukkit.getOnlinePlayers()));
+        }
+        List<Player> players = sender.getNearbyEntities(radius, radius, radius).stream()
+            .filter(entity -> entity instanceof Player)
+            .map(entity -> (Player) entity)
+            .toList();
+        if (players.isEmpty()) {
+            MessageConfig.getInstance().getNoNearbyPlayersMessage().send(sender);
+            return Optional.empty();
+        }
+        return Optional.of(players);
+    }
+
+    private Component processPing(@NotNull Player player, @NotNull Component component) {
+        if (!channel.enablePing() || channel.pingCooldown.hasCooldown(player.getUniqueId())) {
+            return component;
+        }
+        ComponentSingleMessage message = ComponentMessage.componentMessage(component);
+        String pingFormat = "@" + player.getName();
+        if (message.containsString(pingFormat)) {
+            message = message.replace(pingFormat, "<red>@" + player.getName());
+            Sound pingSound = channel.pingSound();
+            if (pingSound != null) {
+                player.playSound(pingSound);
+            }
+            channel.pingCooldown.applyCooldown(player.getUniqueId(), Duration.ofSeconds(channel.pingCooldown()));
+            return message.get();
+        }
+        return component;
+    }
+
+}
