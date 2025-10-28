f5754a2c23e4458917dfbfa432721affbefb6947
FireML <fireml@firedev.uk>
2025-10-28 18:21:08 +0000

    Add JavaDocs to ChatChannel

--- src/main/java/uk/firedev/chatchannels/api/ChatChannel.java
+++ src/main/java/uk/firedev/chatchannels/api/ChatChannel.java
@@ -26,28 +26,58 @@ import java.util.Objects;
 
 public interface ChatChannel extends RegistryItem {
 
+    /**
+     * @return Whether this channel should be enabled or not.
+     */
     boolean isEnabled();
 
+    /**
+     * @return The name of this channel.
+     */
     @NotNull String name();
 
     default @NotNull String getKey() {
         return name();
     }
 
+    /**
+     * @return The plugin that owns this channel.
+     */
     @NotNull Plugin plugin();
 
+    /**
+     * @return How this channel should look to players.
+     */
     @NotNull ComponentSingleMessage display();
 
+    /**
+     * @return If pings should be enabled.
+     */
     boolean enablePing();
 
+    /**
+     * @return The sound a ping should make.
+     */
     @Nullable Sound pingSound();
 
+    /**
+     * @return The cooldown time between pings.
+     */
     int pingCooldown();
 
+    /**
+     * @return The requirements to access this channel.
+     */
     @NotNull Requirement accessRequirement();
 
+    /**
+     * @return The chat format for this channel.
+     */
     @NotNull ComponentSingleMessage format();
 
+    /**
+     * @return The radius for this channel's messages. Set to 0 or below to disable.
+     */
     long radius();
 
     default void handleEvent(@NotNull AsyncChatEvent event) {
@@ -67,10 +97,22 @@ public interface ChatChannel extends RegistryItem {
         new Messaging(this).sendMessage(sender, component, message);
     }
 
+    /**
+     * @param player The player who sent the message.
+     * @param target The player who is receiving the message.
+     * @return If the target player should receive the message.
+     */
     boolean shouldSendToTarget(@NotNull Player player, @NotNull Player target);
 
+    /**
+     * @param player The relevant player.
+     * @return Text replacements for this channel.
+     */
     @Nullable Replacer replacer(@NotNull Player player);
 
+    /**
+     * @return The {@link CooldownHelper} instance to handle ping cooldowns.
+     */
     @NotNull CooldownHelper pingCooldownHandler();
 
     default boolean hasAccess(@NotNull Player player) {
@@ -82,6 +124,9 @@ public interface ChatChannel extends RegistryItem {
         );
     }
 
+    /**
+     * @return The command shortcuts to access this channel.
+     */
     @NotNull List<String> aliases();
 
     default void registerAliases() {
